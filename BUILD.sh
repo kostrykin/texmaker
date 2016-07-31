aaf91b24119b22e68d04de4780833a23e4e8c821
Leonid Kostrykin <void@evoid.de>
2016-07-31 21:26:43 +0200

    makes install in BUILD.sh optional
    Extends BUILD.sh such that it asks whether installation should be performed after compilation.

--- BUILD.sh
+++ BUILD.sh
@@ -5,11 +5,15 @@ echo "Enter path to QT:"
 read QTDIR
 echo "Enter SYSTEM (1: UNIX ; 2: MACOSX) :"
 read SYSTEM
+echo "Install afterwards? (1: YES ; 2: NO) :"
+read INSTALL
 PATH=$QTDIR/bin:$PATH
 LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
 DYLD_LIBRARY_PATH=$QTDIR/lib:$DYLD_LIBRARY_PATH
 export QTDIR PATH LD_LIBRARY_PATH DYLD_LIBRARY_PATH
-if [ "$SYSTEM" = 1 ] 
+if [ "$SYSTEM" = 1 ]
+then
+if [ "$INSTALL" = 1 ]
 then
 echo "Enter PREFIX (/usr , /usr/local or /opt) :"
 read PREFIX
@@ -21,6 +25,11 @@ $QTDIR/bin/qmake PREFIX=$PREFIX DESKTOPDIR=$DESKTOPDIR ICONDIR=$ICONDIR texmaker
 make
 make install
 echo "Compilation and installation done"
+else
+$QTDIR/bin/qmake texmaker.pro
+make
+echo "Compilation done"
+fi
 exit 0
 fi
 
@@ -28,8 +37,13 @@ if [ "$SYSTEM" = 2 ]
 then
 $QTDIR/bin/qmake -macx -spec macx-g++ texmaker.pro
 make
+if [ "$INSTALL" = 1 ]
+then
 make install
 echo "Compilation and installation done"
+else
+echo "Compilation done"
+fi
 exit 0
 fi
 
